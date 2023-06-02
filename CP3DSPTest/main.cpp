2220079ee042554afb2ed8f17086514076e8f6c6
Corné <corne@cp3.io>
2023-06-02 11:12:01 +0200

    Initial Commit

---
+++ CP3DSPTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CP3DSPTest
+//
+//  Created by Corné Driesprong on 02/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
